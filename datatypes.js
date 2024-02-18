8a72b0a61b05bb0b31a0ad51b296f8ac968937d4
Sidharth Mehta <145455338+Sidharth70754@users.noreply.github.com>
2024-02-18 22:59:15 +0530

    Create datatypes.js

---
+++ datatypes.js
@@ -0,0 +1,18 @@
+"use strict";  // treat all JS code as new version 
+
+// alert("Please")
+
+console.log(3+3);
+let name = "sidharth"
+
+let age = 20
+let isloggedin = false
+
+// number 
+// bigint
+// string 
+// boolean = true/false
+// null = standalone value
+// undefined 
+// symbol = unique
+// object 
